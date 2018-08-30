73f6c14668f59eb194883a885fb0f77dfa7861ec
Surendran <surendran@hasura.io>
2018-08-30 15:18:55 +0530

    Styles fixed

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -6,14 +6,24 @@ import moment from 'moment';
 export default class MessageList extends React.Component {
   render() {
     const { isNew } = this.props;
-    
+
     return (
       <div className={isNew ? "messageWrapperNew" : "messageWrapper"}>
         {
           this.props.messages.map((m, i) => {
             return (
               <div key={m.id} className="message">
-                <b>{m.username}</b>: {m.text } <i>{moment(m.timestamp).fromNow()} </i>
+                <div className="messageNameTime">
+                  <div className="messageName">
+                    <b>{m.username}</b>
+                  </div>
+                  <div className="messsageTime">
+                    <i>{moment(m.timestamp).fromNow()} </i>
+                  </div>
+                </div>
+                <div className="messageText">
+                  {m.text }
+                </div>
               </div>
             );
           })
